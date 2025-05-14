6217f60c52a7044c5f07f3e75d57af3d25e7dca2
CezinhaDev <cesarhenriquee04@gmail.com>
2025-05-14 20:48:00 -0300

    feat: app finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from  './logo.svg'; // Importa um arquivo SVG (imagem) chamado 'logo'.
+import './App.css'; // Importa o arquivo CSS para estilizar o componente.
+import Login from './componentes/login';
 
-function App() {
+
+function App() { // Define o componente funcional 'App'.
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Login />
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; // Exporta o componente para ser usado em outros arquivos.
